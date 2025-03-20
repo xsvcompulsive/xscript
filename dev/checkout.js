c0d39176b524eb2264711ed0a874dc3db660942a
xsvcompulsive <xsv.solja@gmail.com>
2025-03-20 11:18:22 +0200

    Add files via upload

--- dev/checkout.js
+++ dev/checkout.js
@@ -1,10 +1,34 @@
-const itemTypes = new Storage();
+const itemTypes = new StorageDict();
 
-const template = document.querySelector(".checkout_item");
+const template = document.querySelector(".checkout-item");
+const removeBtnClone = document.querySelector(".remove-btn");
+const removeBtns = document.getElementsByClassName('remove-btn');
+let totalPrice = 0;
 
-itemTypes.list.forEach(item => {
+for (itemName in itemTypes.all) {
     const clone = template.cloneNode(true);
+    const cloneRemoveBtn = removeBtnClone.cloneNode(true);
+    clone.textContent = itemName;
+    cloneRemoveBtn.itemName = itemName;
+    cloneRemoveBtn.setAttribute("item-name", itemName);
     clone.hidden = false;
-    clone.textContent = item;
+    cloneRemoveBtn.hidden = false;
+    
     template.parentNode.appendChild(clone);
-});
+    template.parentNode.append(cloneRemoveBtn);
+    
+    totalPrice += parseInt(itemTypes.all[itemName]);
+}
+
+if (itemTypes.all != "") {
+    const priceElement = document.querySelector(".price-total");
+    priceElement.textContent = totalPrice;
+    priceElement.hidden = false;
+}
+
+for (let button of removeBtns) {
+    button.addEventListener('click', function () {
+        itemTypes.remove(button.getAttribute('item-name'));
+        //refresh div
+    });
+}
--- dev/session_cart.js
+++ dev/session_cart.js
@@ -1,12 +1,13 @@
 const buttons = document.getElementsByClassName('item_btn');
 
-let item_storage = new Storage();
+let item_storage = new StorageDict();
 //item_storage.clear();
 
 for (let button of buttons) {
     button.addEventListener('click', function () {
-        const itemAttr = button.getAttribute('item-attr');
-        item_storage.add(itemAttr);
+        const itemName = button.getAttribute('item-name');
+        const itemPrice = button.getAttribute('item-price');
+        item_storage.add(itemName, itemPrice);
     });
 }
 
